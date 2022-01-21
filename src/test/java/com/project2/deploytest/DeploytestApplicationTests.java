c9c025843852e926e4945686ac73b0720f48a33b
Yandira Delgado <76410736+yandirad@users.noreply.github.com>
2022-01-21 10:18:49 -0300

    Update DeploytestApplicationTests.java

--- src/test/java/com/project2/deploytest/DeploytestApplicationTests.java
+++ src/test/java/com/project2/deploytest/DeploytestApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DeploytestApplicationTests {
+class DeploytestApplicationTests 
 
 	@Test
 	void contextLoads() {
